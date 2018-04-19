ced942017a297ef4d41fc155ac3f61a7ae1f2d9e
wilbert.waller <wilbert.waller@polarisalpha.com>
2018-04-19 12:25:00 -0600

    Reduce number of variables

--- script.js
+++ script.js
@@ -9,21 +9,20 @@ fs.readFile('./advent_of_code_input.txt', 'utf8', (err, data) => {
 
 	// Part 1 -- Find what floor the instructions take Santa
 
-	var up = 0;
-	var down = 0;
-	var position = 0;
+	let floor = 0;
+	let position = 0;
 
-	for (var i = 0; i < data.length; i++) {
-		data[i] === '(' ? up++ : down++;
+	for (let i = 0; i < data.length; i++) {
+		data[i] === '(' ? floor++ : floor--;
 
 		// Part 2 -- Find the position in instructions that first takes Santa to floor -1
 
-		if ((up - down === -1) && position === 0) {
+		if (floor === -1 && position === 0) {
 			position = i + 1;
 		}
 	}
 	
-	console.log(`Santa ends up at floor ${up - down}.`);
+	console.log(`Santa ends up at floor ${floor}.`);
 	console.log(`Santa first enters the basement at position ${position}.`);
 
 	console.timeEnd('Challenge Time');
