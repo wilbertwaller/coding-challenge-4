227a2939bfe0695da3d12412da9dfd8fa4037bcb
wilbert.waller <wilbert.waller@polarisalpha.com>
2018-04-27 12:14:06 -0600

    Separate parts to compare execution times

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 const fs = require('fs');
 
 fs.readFile('./advent_of_code_input.txt', 'utf8', (err, data) => {
-	console.time('Challenge Time');
-
 	if (err) {
-		return console.log('Error reading file:', err);
+		return console.log('Error reading file');
 	}
 
-	// Part 1 -- Find what floor the instructions take Santa
+	console.time('Part 1 Time');
+	doPartOne(data);
+	console.timeEnd('Part 1 Time');
 
+	console.time('Part 2 Time');
+	doPartTwo(data);
+	console.timeEnd('Part 2 Time');
+});
+
+// Part 1 -- Find what floor the instructions take Santa
+const doPartOne = (data) => {
+	let up = data.match(/\(/g).length;
+	
+	console.log(`Santa ends up at floor ${up - (data.length - up)}.`);
+};
+
+// Part 2 -- Find the position in instructions that first takes Santa to floor -1
+const doPartTwo = (data) => {
 	let floor = 0;
-	let position = 0;
 
 	for (let i = 0; i < data.length; i++) {
 		data[i] === '(' ? floor++ : floor--;
 
-		// Part 2 -- Find the position in instructions that first takes Santa to floor -1
-
-		if (floor === -1 && position === 0) {
-			position = i + 1;
+		if (floor === -1) {
+			return console.log(`Santa first enters the basement at position ${i + 1}.`);		
 		}
 	}
-	
-	console.log(`Santa ends up at floor ${floor}.`);
-	console.log(`Santa first enters the basement at position ${position}.`);
-
-	console.timeEnd('Challenge Time');
-});
+};
